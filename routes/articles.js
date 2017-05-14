ab75db93a5f06e20fc723be0dee22f313fc7761c
Hovhannes Sanoyan <hovosanoyan@gmail.com>
2017-05-14 19:03:43 +0400

    Make css and js inline.

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const albums = require('../lib/albums')
+const fs = require('fs')
+const path = require('path')
+
+const css = fs.readFileSync(path.join(__dirname, '../public/stylesheets/styles.css'), 'utf8').replace(/\s+/g, '')
+const js = fs.readFileSync(path.join(__dirname, '../public/javascripts/main.js'), 'utf8').replace(/\s+/g, '')
 
 const articles = {
   'vardan-jaloyan-body-investment': 'vjbi',
@@ -14,6 +19,8 @@ router.get('/:articleName', function (req, res, next) {
   const articleName = req.params.articleName
   if (!articleName) return next()
   res.render('articles/' + articles[articleName] + '-' + req.getLocale(), {
+    js,
+    css,
     albums,
     i18n: res,
     url: `/articles/${articleName}`
@@ -22,6 +29,8 @@ router.get('/:articleName', function (req, res, next) {
 
 router.get('/', function (req, res, next) {
   res.render('articles/articles-' + req.getLocale(), {
+    js,
+    css,
     albums,
     i18n: res,
     url: '/articles'
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const albums = require('../lib/albums')
 const imageManager = require('../lib/image-manager')
+const fs = require('fs')
+const path = require('path')
+
+const css = fs.readFileSync(path.join(__dirname, '../public/stylesheets/styles.css'), 'utf8').replace(/\s+/g, ' ')
+const js = fs.readFileSync(path.join(__dirname, '../public/javascripts/main.js'), 'utf8').replace(/\s+/g, ' ')
 
 router.get('/:album/:imageName?', (req, res, next) => {
   const album = req.params.album
@@ -12,6 +17,8 @@ router.get('/:album/:imageName?', (req, res, next) => {
   const albumImages = endYear ? imageManager.getByYears([startYear, endYear]) : imageManager.getByYear([startYear])
   const selectedImage = albumImages.find(img => img.fileName === (imageName + '.jpg')) || albumImages[0]
   res.render('album', {
+    js,
+    css,
     albums,
     i18n: res,
     albumImages,
@@ -22,19 +29,19 @@ router.get('/:album/:imageName?', (req, res, next) => {
 })
 
 router.get('/biography', (req, res, next) => {
-  res.render('biography-' + req.getLocale(), { albums, i18n: res, url: req.url })
+  res.render('biography-' + req.getLocale(), { albums, i18n: res, url: req.url, css, js })
 })
 
 router.get('/exhibitions', (req, res, next) => {
-  res.render('exhibitions', { albums, i18n: res, url: req.url })
+  res.render('exhibitions', { albums, i18n: res, url: req.url, css, js })
 })
 
 router.get('/contacts', (req, res, next) => {
-  res.render('contacts', { albums, i18n: res, url: req.url })
+  res.render('contacts', { albums, i18n: res, url: req.url, css, js })
 })
 
 router.get('/', (req, res, next) => {
-  res.render('index', { albums, i18n: res, url: req.url })
+  res.render('index', { albums, i18n: res, url: req.url, css, js })
 })
 
 module.exports = router
