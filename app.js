50429b32e8cddc648e0376e7893575bbdffde895
Hovhannes Sanoyan <hovosanoyan@gmail.com>
2017-05-14 18:02:49 +0400

    Add compression and don't add logger in production mode.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const i18n = require('i18n')
+const compression = require('compression')
 
 const index = require('./routes/index')
 const articles = require('./routes/articles')
@@ -22,6 +23,7 @@ i18n.configure({
 app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/biography'), path.join(__dirname, 'views/articles')])
 app.set('view engine', 'pug')
 
+app.use(compression())
 app.use(i18n.init)
 app.use((req, res, next) => {
   const match = req.url.match(/^\/([A-Z]{2})([/?].*)?$/i)
@@ -34,7 +36,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(logger('dev'))
+process.env.NODE_ENV !== 'production' && app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -45,14 +47,14 @@ app.use('/', index)
 app.use('/articles', articles)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
