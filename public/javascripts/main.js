e51841f4af10f5c7332216fe3f6ae1c308d4c7eb
Hovhannes Sanoyan <hovosanoyan@gmail.com>
2017-05-14 14:46:51 +0400

    Implemented gallery.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,46 @@
-/* global $ */
+/* global $,  */
 
  $(function () {
    $('.menu').fadeIn(0)
-   matchAndMarkSelectedAlbum()
+   matchAndMarkSelectedPage()
+   $('.hamburger').click(function () {
+     $('.hamburger').toggleClass('show-menu')
+   })
  })
 
- function matchAndMarkSelectedAlbum () {
-   var startYear = window.location.href.match(/20\d{2}/)
-   var selectedLink = $('a[href*="' + startYear + '"]')
-   selectedLink.addClass('selected')
+ window.nextImage = nextImage
+ window.previousImage = previousImage
+
+ function nextImage () {
+   var currentImage = getCurrentImage()
+   var indexOfNext = window.albumImages.indexOf(currentImage) + 1
+   if (!window.albumImages[indexOfNext]) indexOfNext = 0
+   var nextImage = window.albumImages[indexOfNext]
+   var newLink = window.location.href.replace(currentImage.fileName.replace('.jpg', ''), nextImage.fileName.replace('.jpg', ''))
+   window.location.replace(newLink)
+ }
+
+ function previousImage () {
+   var currentImage = getCurrentImage()
+   var indexOfPrevious = window.albumImages.indexOf(currentImage) - 1
+   if (!window.albumImages[indexOfPrevious]) indexOfPrevious = (window.albumImages.length - 1)
+   var prevImage = window.albumImages[indexOfPrevious]
+   var newLink = window.location.href.replace(currentImage.fileName.replace('.jpg', ''), prevImage.fileName.replace('.jpg', ''))
+   window.location.replace(newLink)
+ }
+
+ function matchAndMarkSelectedPage () {
+   var links = $('nav a[href]')
+   for (var i = (links.length - 1); i >= 0; i--) {
+     var a = links[i]
+     var href = $(a).attr('href').replace(window.location.hostname)
+     href = href.match(/20\d{2}/) || href
+     if (window.location.href.indexOf(href) !== -1) return $(a).addClass('selected')
+   }
+   return $($('nav a')[0]).addClass('selected')
+ }
+
+ function getCurrentImage () {
+   var link = $('.image-container img').attr('src')
+   return window.albumImages.find(img => link.indexOf(img.fileName) > -1)
  }
