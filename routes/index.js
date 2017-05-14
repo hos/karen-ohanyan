f9c38dffbc124a1fafdb16cfb8d5491663d05ca7
Hovhannes Sanoyan <hovosanoyan@gmail.com>
2017-05-14 21:51:45 +0400

    schema

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,15 @@ const css = fs.readFileSync(path.join(__dirname, '../public/stylesheets/styles.c
 const js = fs.readFileSync(path.join(__dirname, '../public/javascripts/main.js'), 'utf8')
 const bootstrap = fs.readFileSync(path.join(__dirname, '../public/stylesheets/bootstrap.min.css'), 'utf8').replace(/\s+/g, ' ')
 
-router.get('/:album/:imageName?', (req, res, next) => {
-  const album = req.params.album
+router.get('/:albumName/:imageName?', (req, res, next) => {
+  const albumName = req.params.albumName
   const imageName = req.params.imageName
-  const startYear = parseInt(album.split('-')[0], 10)
+  const startYear = parseInt(albumName.split('-')[0], 10)
   if (!startYear) return next()
-  const endYear = parseInt(album.split('-')[1], 10)
+  const endYear = parseInt(albumName.split('-')[1], 10)
   const albumImages = endYear ? imageManager.getByYears([startYear, endYear]) : imageManager.getByYear([startYear])
   const selectedImage = albumImages.find(img => img.fileName === (imageName + '.jpg')) || albumImages[0]
+  const album = albums.find(album => album.href.includes(albumName))
   res.render('album', {
     currentUrl: req.url.replace('/' + imageName, ''),
     selectedImage,
@@ -25,6 +26,7 @@ router.get('/:album/:imageName?', (req, res, next) => {
     i18n: res,
     bootstrap,
     albums,
+    album,
     css,
     js
   })
