071369822ea5a7ee174f6adcbfc2ab665c16fe6e
Hovhannes Sanoyan <hovosanoyan@gmail.com>
2017-05-17 12:06:01 +0400

    Error handler.

--- routes/index.js
+++ routes/index.js
@@ -77,4 +77,27 @@ router.get('/articles/', function (req, res, next) {
   })
 })
 
+// catch 404 and forward to error handler
+router.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+router.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error', {
+    i18n: res,
+    albums,
+    css,
+    js
+  })
+})
+
 module.exports = router
